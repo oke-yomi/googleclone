38d57e5fad37299e805395db5b1543292eb15930
oke-yomi <okeoluwayomi9@gmail.com>
2021-10-16 21:39:26 +0100

    working project

---
+++ next.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['www.google.co.uk', 'www.unsplash.com']
+  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: 'https://google.com/search?q=${term}',
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: '/',
+  //       destination: 'https://perfects.engineering',
+  //       permanent: true,
+  //     },
+  //   ]
+  // }
+}
